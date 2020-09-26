b642aa844d6da9847e233d81ae6caccc4781d58d
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-26 01:54:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,8 +13,10 @@ class Song
   end
 
   def artist_name
-    self.artist.name
-    nil if self.artist.name != true
+    if self.artist == true
+      self.srtist.name
+    else
+      nil
   end
 
 end
