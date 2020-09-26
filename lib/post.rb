e5a53eef6a49892c64b5e9b3e51672a782bc2d49
Amanda Mongiove <amandamongiove@gmail.com>
2020-09-26 01:43:47 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+
+  attr_accessor :title, :author
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    #title.author = Author.add_post(title)
+  end
+
+
+end
